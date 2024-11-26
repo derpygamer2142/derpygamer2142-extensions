cb1eb201710c2608236afd128e68eb4c7b52eb49
derpygamer2142 <93667155+derpygamer2142@users.noreply.github.com>
2024-11-25 20:12:30 -0800

    whoops forgot this part

--- src/lib/assets/images/assetList.js
+++ src/lib/assets/images/assetList.js
@@ -2,9 +2,6 @@ import SoupCat from "./soup-cat.gif"
 import Rickroll from "./rickroll.mp4"
 import AdapterFail from "./adapter fail.png"
 import FinishedBasics from "./finishedbasics.svg"
-import DoublingDemo from "/doublingdemo.sb3"
-import AtomicDemo from "/atomicDemo.sb3"
-import TextureDemo from "/textureDemo.sb3"
 import GPUSb3Thumbnail from "./gpusb3 thumbnail.png"
 
 export const assets = {
@@ -12,8 +9,5 @@ export const assets = {
     Rickroll: Rickroll,
     AdapterFail: AdapterFail,
     FinishedBasicsImg: FinishedBasics,
-    FinishedBasicsProject: DoublingDemo,
     GPUSb3Thumbnail: GPUSb3Thumbnail,
-    AtomicDemo: AtomicDemo,
-    TextureDemo: TextureDemo
 }
